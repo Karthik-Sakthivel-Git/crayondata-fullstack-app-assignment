b42322fe78ece3f3e57101960ee76c9abe596368
Karthik Sakthivel <skarthik333@gmail.com>
2025-10-01 23:16:10 +0530

    Custom security group deployed in custom vpc for ssh, http and https

---
+++ infra/sg.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "custom-web-sg" {
+  name        = "${local.name}-web-SG"
+  description = "Allow SSH HTTP and HTTPS inbound"
+  vpc_id      = module.custom-vpc.vpc_id
+
+  ingress {
+    description = "SSH inbound"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    description = "HTTP inbound"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["157.51.144.185/32"]
+  }
+
+  ingress {
+    description = "HTTPS inbound"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(local.tags, {
+    Name = "${local.name}-web-sg"
+  })
+}
