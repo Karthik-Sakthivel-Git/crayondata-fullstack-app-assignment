4fd2aff7fe18d2c8548204e7bf7d36ecf38a3f4c
Karthik Sakthivel <skarthik333@gmail.com>
2025-10-02 15:48:57 +0530

    update: user data removed part of locals.tf file

--- infra/locals.tf
+++ infra/locals.tf
@@ -5,11 +5,6 @@ locals {
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  user_data = <<-EOT
-    #!/bin/bash
-    echo "Hello Terraform!"
-  EOT
-
   tags = {
     Environment = "Test"
     Repository = "https://github.com/Karthik-Sakthivel-Git/crayondata-fullstack-app-assignment.git"
